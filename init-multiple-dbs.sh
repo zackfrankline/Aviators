3172a9c3f86e24a56ad95572179e43363d72af64
zackfrankline <zackfrankline@gmail.com>
2026-09-14 07:35:40 +0530

    updated deployment steps to qa #4

--- init-multiple-dbs.sh
+++ init-multiple-dbs.sh
@@ -4,4 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE content_db;
     CREATE DATABASE subscription_db;
+    CREATE DATABASE auth_db;
 EOSQL
